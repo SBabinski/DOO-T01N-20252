acaa8eab583e160e753f2cd0a696b87159ec0ab2
netobjj <joseeuclides18@gmail.com>
2025-10-14 20:14:06 -0300

    cleanup

--- Alunos/Jose_Euclides/aula_09/Main.java
+++ Alunos/Jose_Euclides/aula_09/Main.java
@@ -18,7 +18,7 @@ public class Main {
         JFrame frame = new JFrame(title);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
-        frame.setLocationRelativeTo(null); // Centraliza a janela na tela
+        frame.setLocationRelativeTo(null);
 
         return frame;
     }
