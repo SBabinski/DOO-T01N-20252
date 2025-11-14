6626ccd4050ec6bfe4280a24de96123eecfa0582
TalesRL <talesleite.r@gmail.com>
2025-11-14 20:03:36 -0300

    aula12!

--- Alunos/Tales/Aula-12/Main.java
+++ Alunos/Tales/Aula-12/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("pt", "BR"));
 
-        // ATV1 - Filtrar números pares
+      
         System.out.println("ATV1");
         var numeros = List.of(3, 8, 15, 22, 7, 10, 4, 19, 2, 31);
         var pares = numeros.stream()
@@ -20,7 +20,7 @@ public class Main {
         System.out.println("Pares: " + pares);
         System.out.println();
 
-        // ATV2 - Converter nomes para maiúsculas
+      
         System.out.println("ATV2");
         var nomes = List.of("roberto", "josé", "caio", "vinicius");
         var nomesMaiusculos = nomes.stream()
@@ -30,7 +30,7 @@ public class Main {
         System.out.println("Maiúsculas: " + nomesMaiusculos);
         System.out.println();
 
-        // ATV3 - Contar ocorrências de palavras
+
         System.out.println("ATV3");
         var palavras = List.of("se", "talvez", "hoje", "sábado", "se", "quarta", "sábado");
         var contagem = palavras.stream()
@@ -40,7 +40,7 @@ public class Main {
                 .forEach(e -> System.out.println(e.getKey() + " -> " + e.getValue()));
         System.out.println();
 
-        // ATV4 - Filtrar produtos > R$100
+     
         System.out.println("ATV4");
         var produtos = List.of(
                 new Produto("Fone de Ouvido", 89.90),
@@ -55,7 +55,7 @@ public class Main {
         System.out.println("Filtrados (> R$100): " + acimaDe100);
         System.out.println();
 
-        // ATV5 - Soma total dos produtos
+        
         System.out.println("ATV5");
         var soma = produtos.stream()
                 .mapToDouble(Produto::preco)
@@ -63,7 +63,6 @@ public class Main {
         System.out.printf("Valor total dos produtos: R$ %.2f%n", soma);
         System.out.println();
 
-        // ATV6 - Ordenar linguagens por tamanho
         System.out.println("ATV6");
         var linguagens = List.of("Java", "Python", "C", "JavaScript", "Ruby");
         var ordenadas = linguagens.stream()
